59482c52f0683b67eaa8227c2aa5ee1974ff32ce
Ian Vredenburg <ian@vredenburg.dev>
2022-07-11 22:51:55 +0200

    Add linter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "bootswatch/dist/minty/bootstrap.min.css";
 
-import App from './App.svelte'
+import App from './App.svelte';
 
 const app = new App({
   target: document.getElementById('app')
